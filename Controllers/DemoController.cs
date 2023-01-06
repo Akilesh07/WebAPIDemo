43d96e35dc2da4f6396f434b48577a440dd27e6d
akilesh.kumar <akilesh.kumar@zohocorp.com>
2023-01-06 19:41:40 +0530

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoDataAccess;
+
+namespace WebAPIDemo.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public IEnumerable<logintable> GetDetails()
+        {
+            using(BookListCrudEntities entities = new BookListCrudEntities())
+            {
+                return entities.logintables.ToList();
+            }
+        }
+
+        public logintable GetDetails(int id)
+        {
+            using(BookListCrudEntities entities = new BookListCrudEntities())
+            {
+                return entities.logintables.FirstOrDefault(e => e.UserId == id);
+            }
+        }
+
+        public void Post([FromBody]logintable LoginTable)
+        {
+            using(BookListCrudEntities entities = new BookListCrudEntities())
+            {
+                entities.logintables.Add(LoginTable);
+                entities.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (BookListCrudEntities entities = new BookListCrudEntities())
+            {
+                entities.logintables.Remove(entities.logintables.FirstOrDefault(e => e.UserId == id));
+                entities.SaveChanges();
+            }
+        }
+    }
+}
